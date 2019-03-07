84ef7c3dc42c883eefc0bd4bfc8fcd06060bd8b4
pepesan <pepesan@gmailcom>
2019-03-07 19:47:23 +0100

    ihncludos ejemplos de classes y herencia en ES6

--- js/clases.js
+++ js/clases.js
@@ -459,3 +459,32 @@ var miSuv=new Suv();
 console.log(miSuv);
 miSuv=new Suv(12,"Toyota",4);
 console.log(miSuv);
+
+
+//ECMASCRIPT 6
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString() {
+        return '(' + this.x + ', ' + this.y + ')';
+    }
+}
+
+class ColorPoint extends Point {
+    constructor(x, y, color) {
+        super(x, y);
+        this.color = color;
+    }
+    toString() {
+        return super.toString() + ' in ' + this.color;
+    }
+}
+
+let cp = new ColorPoint(25, 8, 'green');
+cp.toString(); // '(25, 8) in green'
+
+console.log(cp instanceof ColorPoint); // true
+console.log(cp instanceof Point); // true
