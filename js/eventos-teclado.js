79bd914c4773b646264cbfed8ad9645e0a746d24
pepesan <pepesan@gmailcom>
2019-03-12 19:56:34 +0100

    ejemplos de eventos de teclado

---
+++ js/eventos-teclado.js
@@ -0,0 +1,38 @@
+/*
+ * Código de ejemplo de el lanzamiento
+ * de una alerta mediante Javascript
+ */
+/*
+Referencia: https://developer.mozilla.org/es/docs/Web/API/KeyboardEvent
+ */
+
+function init(){
+    console.log('La página se ha cargado completamente');
+}
+window.addEventListener("load", init);
+//window.onload=init;
+
+function foco(evento) {
+    console.log(evento.currentTarget+" tiene el foco");
+}
+
+function teclaPulsada(evento) {
+    console.log(evento.currentTarget.value);
+    console.log(evento.keyCode);
+}
+
+function pierdefoco(evento) {
+    console.log(evento.currentTarget+" ya no tiene el foco");
+}
+
+function domCargado(event) {
+    console.log("El DOM se ha cargado completamente");
+    var campo= document.getElementById("username");
+    campo.addEventListener("focus",foco);
+    campo.addEventListener("blur",pierdefoco);
+    campo.addEventListener("keydown",teclaPulsada);
+    campo.addEventListener("keypress",teclaPulsada);
+    campo.addEventListener("keyup",teclaPulsada);
+}
+document.addEventListener("DOMContentLoaded", domCargado);
+
