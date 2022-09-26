b1d4ecc64a6a7272d3f54a1a2fd4bde8ea0c5120
David Vaquero <pepesan@gmail.com>
2022-09-26 13:59:09 +0200

    Mejora de los ejemplos de uso de definición y uso de Objetos

--- js/objetos.js
+++ js/objetos.js
@@ -6,7 +6,11 @@ var arrayVacio=[];
 var objetoVacio={
 
 };
+// variable función
+var miFunction = function (){}
+
 var objeto={
+    // propiedades o atributos
     nombre: "David",
     apellido: "Vaquero",
     ciudad: "Salamanca",
@@ -14,6 +18,7 @@ var objeto={
     edad:37,
     activo:true,
     cursos:["Javascript","HTML","CSS"],
+    // propiedad de tipo función, ó método
     toString: function(){
         //console.log(objeto);
         //console.log(objeto.nombre);
@@ -39,6 +44,7 @@ var objeto={
 };
 console.log(objeto);
 console.log(objeto.nombre);
+console.log(objeto.apellido);
 objeto.nombre="Pepe";
 console.log(objeto.nombre);
 console.log(objeto.toString());
@@ -53,3 +59,56 @@ objeto.addCurso("CSS2");
 console.log(objeto.cursos);
 console.log(objeto.direccion);
 console.log(objeto.direccion.calle);
+
+
+var cliente = {
+    nombre: "Verónica",
+    nif: "07977851A",
+    email: "pepesan@gmail.com",
+    tlf: "923123456",
+    direccion: {
+        calle: "Calle Mayor",
+        numero: "19"
+    },
+    serviciosContratados: []
+}
+
+console.log(cliente);
+console.log(cliente.nombre);
+console.log(cliente.nif);
+console.log(cliente.email);
+console.log(cliente.direccion.calle);
+console.log(cliente.direccion.numero);
+cliente.nombre = "Pepito";
+cliente.direccion.numero = "20";
+
+var coche = {
+    marca: "Toyota",
+    modelo: "Auris"
+}
+console.log(coche.marca);
+console.log(coche.modelo);
+coche.modelo = "Corolla";
+
+coche = {
+    _marca: "Toyota",
+    _modelo: "Auris",
+    getMarca: function (){
+        return this._marca;
+    },
+    setMarca: function (marca) {
+        this._marca = marca;
+    },
+    getModelo: function (){
+        return this._modelo;
+    },
+    setModelo: function (modelo) {
+        this._modelo = modelo;
+    }
+}
+// getter
+console.log(coche.getMarca())
+// setter
+coche.setMarca("Audi");
+console.log(coche.getMarca());
+
