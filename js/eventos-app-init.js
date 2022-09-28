daaaea5b42043615d82510ca0773a8b94141adbc
David Vaquero <pepesan@gmail.com>
2022-09-28 11:59:35 +0200

    Mejora de los ejemplos de uso de eventos

--- js/eventos-app-init.js
+++ js/eventos-app-init.js
@@ -3,8 +3,14 @@
     Licencia: GPLv3
     Descripción: Programa de ejemplo de Javascript
  */
+
+function muestraSegundoMensaje(){
+    console.log("Segundo Mensaje");
+}
 function init (){
     console.log("DOM Cargado");
     document.getElementById("pinchable2").onclick = muestraMensaje;
+    document.getElementById("pinchable3").addEventListener("click", muestraMensaje);
+    document.getElementById("pinchable3").addEventListener("click", muestraSegundoMensaje);
 }
 document.addEventListener("DOMContentLoaded", init);
