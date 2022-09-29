30024b4ed35c79575766336392db9322ac3b4fad
David Vaquero <pepesan@gmail.com>
2022-09-29 10:16:43 +0200

    Mejora de los ejemplos de uso de eventos y ejercicios asociados

--- ejercicios/eventos-previos-17.js
+++ ejercicios/eventos-previos-17.js
@@ -8,15 +8,38 @@ function onMouseOut(){
     console.log("Fuera");
 }
 function onMouseMove(event){
-    //console.log(event);
+    // console.log(event);
     console.log("posicion: x: "+event.clientX+", y: "+event.clientY+")");
+}
+// definición de función callback como array function o lambda
+var onMouseMovev2 = (event)=> {
+    console.log(event);
 }
 function init(){
     console.log("Dom Cargado");
     document.getElementById("enviar").addEventListener("click", clicked);
     document.getElementById("boton-raton").addEventListener("mouseover", onMouseOver);
     document.getElementById("boton-raton").addEventListener("mouseout", onMouseOut);
-    document.getElementById("boton-raton").addEventListener("mousemove", onMouseMove);
+    //document.getElementById("boton-raton").addEventListener("mousemove", onMouseMove);
+    /*
+    document
+        .getElementById("boton-raton")
+        .addEventListener(
+            "mousemove",
+            function (event) {
+                console.log(event);
+            }
+        );
+     */
+    document
+        .getElementById("boton-raton")
+        .addEventListener(
+            "mousemove",
+            (event)=> {
+                console.log(event);
+            }
+        );
+
 }
 function loaded(){
     console.log("Completamente Cargada");
