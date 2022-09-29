e1c3a77ebcf03fc81a252f8e62727a822936ba7d
David Vaquero <pepesan@gmail.com>
2022-09-29 09:58:54 +0200

    Mejora de los ejemplos de uso de eventos y ejercicios asociados

--- ejercicios/eventos-previos-17.js
+++ ejercicios/eventos-previos-17.js
@@ -7,11 +7,16 @@ function onMouseOver(){
 function onMouseOut(){
     console.log("Fuera");
 }
+function onMouseMove(event){
+    //console.log(event);
+    console.log("posicion: x: "+event.clientX+", y: "+event.clientY+")");
+}
 function init(){
     console.log("Dom Cargado");
     document.getElementById("enviar").addEventListener("click", clicked);
     document.getElementById("boton-raton").addEventListener("mouseover", onMouseOver);
     document.getElementById("boton-raton").addEventListener("mouseout", onMouseOut);
+    document.getElementById("boton-raton").addEventListener("mousemove", onMouseMove);
 }
 function loaded(){
     console.log("Completamente Cargada");
