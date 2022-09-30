b4950571b50fd4f6ff2c7c9cd535aa80151176c7
David Vaquero <pepesan@gmail.com>
2022-09-30 10:08:44 +0200

    Mejora de los ejemplos de uso de petición http con fetch con manejo de Promesas

---
+++ js/promesas.js
@@ -0,0 +1,24 @@
+/*
+    Plantilla de uso de eventos en JS
+ */
+function init(){
+    console.log("Dom Cargado");
+    const fetchPromise = fetch('https://cursosdedesarrollo.com/pactometro/resultados.json');
+
+    console.log(fetchPromise);
+
+    fetchPromise.then((response) => {
+        console.log(`Received response: ${response.status}`);
+    }).catch(error => {
+        console.log(error);
+    });
+
+    console.log("Started request…");
+
+}
+function loaded(){
+    console.log("Completamente Cargada");
+}
+
+document.addEventListener("DOMContentLoaded", init);
+window.addEventListener("load", loaded);
