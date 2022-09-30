18eadf849211d0f544d58082c95c4652bbd839dc
David Vaquero <pepesan@gmail.com>
2022-09-30 10:03:36 +0200

    Mejora de los ejemplos de uso de XMLHttpRequest

---
+++ js/xmlhttprequest.js
@@ -0,0 +1,43 @@
+/*
+    Plantilla de uso de eventos en JS
+ */
+function init(){
+    console.log("Dom Cargado");
+    // 1. Crea un nuevo objeto XMLHttpRequest
+    let xhr = new XMLHttpRequest();
+
+    // 2. Configuración: solicitud GET para la URL /article/.../load
+    xhr.open('GET', 'https://cursosdedesarrollo.com/pactometro/resultados.json');
+
+    // 3. Envía la solicitud a la red
+    xhr.send();
+
+    // 4. Esto se llamará después de que la respuesta se reciba
+    xhr.onload = function() {
+        if (xhr.status !== 200) { // analiza el estado HTTP de la respuesta
+            console.log(`Error ${xhr.status}: ${xhr.statusText}`); // ej. 404: No encontrado
+        } else { // muestra el resultado
+            console.log(`Hecho, obtenidos ${xhr.response.length} bytes`); // Respuesta del servidor
+            console.log(xhr.response);
+        }
+    };
+
+    xhr.onprogress = function(event) {
+        if (event.lengthComputable) {
+            console.log(`Recibidos ${event.loaded} de ${event.total} bytes`);
+        } else {
+            console.log(`Recibidos ${event.loaded} bytes`); // sin Content-Length
+        }
+
+    };
+
+    xhr.onerror = function() {
+        console.log("Solicitud fallida");
+    };
+}
+function loaded(){
+    console.log("Completamente Cargada");
+}
+
+document.addEventListener("DOMContentLoaded", init);
+window.addEventListener("load", loaded);
