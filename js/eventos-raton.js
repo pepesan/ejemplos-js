93824c74639d741d268420e6f978ef3970981eb3
David Vaquero <pepesan@gmail.com>
2022-10-11 12:08:02 +0200

    Mejora de los ejemplos de eventos de ratón

--- js/eventos-raton.js
+++ js/eventos-raton.js
@@ -3,36 +3,31 @@
  * de una alerta mediante Javascript
  */
 
-
 function init(){
     console.log('La página se ha cargado completamente');
 }
 window.addEventListener("load", init);
 //window.onload=init;
 
-function cambiaColor(event) {
-    console.log("Cambiacolor");
-    if(event.currentTarget.className!="box colorea"){
-        event.currentTarget.className="box colorea"
-    }else{
-        event.currentTarget.className="box"
-    }
-
-}
-function muestraCoordenadas(event) {
-    console.log("x: "+event.clientX+", y: "+event.clientY);
-}
-
 function domCargado(event) {
     console.log("El DOM se ha cargado completamente");
-    var cajas = document.querySelectorAll(".box");
-    console.log(cajas);
-    for (var i=0;i<cajas.length;i++){
-        console.log(cajas[i]);
-        cajas[i].addEventListener("mouseover",cambiaColor);
-        cajas[i].addEventListener("mousemove",muestraCoordenadas);
-        cajas[i].addEventListener("mouseout",cambiaColor);
-    }
+    var boton = document.getElementById("raton");
+    // mouseover = entro en un elemento
+    boton.addEventListener("mouseover", ()=>{
+        console.log("mouseover");
+    });
+    // mouseover = salgo en un elemento
+    boton.addEventListener("mouseout", ()=>{
+        console.log("mouseout");
+    });
+    // mouseover = cada vez que muevo sobre en un elemento
+    boton.addEventListener("mousemove", (evento)=>{
+        console.log("mousemove:" + "X: "+ evento.pageX + ": Y: "+ evento.pageY);
+    });
+    var botonFormulario = document.getElementById("boton-formulario");
+    botonFormulario.addEventListener("click", () => {
+        console.log("boton de formulario pulsado");
+    })
 }
 document.addEventListener("DOMContentLoaded", domCargado);
 
