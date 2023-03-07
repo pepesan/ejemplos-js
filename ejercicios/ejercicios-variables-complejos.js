edf0ecbb2240c565257960a5f3ea8b0c6100a26f
David Vaquero <pepesan@gmail.com>
2023-03-07 10:00:15 +0100

    resuelto el ejercicio de variables complejas

---
+++ ejercicios/ejercicios-variables-complejos.js
@@ -0,0 +1,50 @@
+/*
+Resuelve los siguientes ejercicios dentro del fichero variables.html
+
+* coloca una etiqueta script dentro de body al final de body
+* define una variable de tipo entero y asígnale el valor 2
+* imprime por consola el valor de la variable 
+* Declara e inicializa una variable de tipo cadena
+* imprime la cadena por consola 
+* imprime el número de caracteres de la variable por consola
+* declara una variable de tipo cadena que contenga como valor un número
+* parsea la variable a número entero
+* imprime la variable por consola
+* declara dos variables de tipo cadena
+* concatena esos dos variables y almacena su valor en otra variable
+* imprime esta última variable por consola
+* imprime esta última variable por consola en mayúsculas
+* y luego en minúsculas
+*/
+
+// define una variable de tipo entero y asígnale el valor 2
+
+var entero = 2;
+// imprime por consola el valor de la variable 
+console.log(entero);
+// Declara e inicializa una variable de tipo cadena
+var cadena = "Chus";
+// imprime la cadena por consola
+console.log(cadena);
+// imprime el número de caracteres de la variable por consola
+console.log(cadena.length);
+// declara una variable de tipo cadena que contenga como valor un número
+var cadenaNumero = "12";
+// parsea la variable a número entero
+var numeroEntero = parseInt(cadenaNumero);
+// imprime la variable por consola
+console.log(numeroEntero);
+// declara dos variables de tipo cadena
+var cadena1 = "Cadena1";
+var cadena2 = "Cadena2";
+// concatena esos dos variables y almacena su valor en otra variable
+var resultado = cadena1 + cadena2;
+// imprime esta última variable por consola
+console.log(resultado);
+// imprime esta última variable por consola en mayúsculas
+console.log(resultado.toUpperCase());
+// y luego en minúsculas
+console.log(resultado.toLowerCase());
+
+
+
